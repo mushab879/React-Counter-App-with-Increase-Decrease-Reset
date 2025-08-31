0d8d6b8974619a2204deacf1d872c9f708601469
mushab879 <mmushab37@gmail.com>
2025-08-31 10:36:26 +0530

    Update counter.js

--- src/counter.js
+++ src/counter.js
@@ -1,12 +1,14 @@
 import data from './data.json';
 import { useState } from 'react';
+import './Counter.css';  // relative path to the CSS file
+
 
 
 function Counter() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div className="counter-container">
       <p>Count: {count}</p>
       <button onClick={() => setCount(count + 1)}>Increase</button>
     </div>
